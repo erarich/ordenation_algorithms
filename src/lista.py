c2adcff44e6d2adcb9b6e4b4d980e95bc3121283
Daniel Vinicius <vinicius.daniel@uft.edu.br>
2024-05-30 13:02:06 -0300

    Arquivo lista.py corrigido

--- src/lista.py
+++ src/lista.py
@@ -1,10 +1,11 @@
 import random
+import os
 
 def gerar_lista(tamanho, tipo):
     if tipo == 'ordenada':
         return list(range(tamanho))
     elif tipo == 'inversa':
-        return list(range(tamanho, 0, -1))
+        return list(range(tamanho - 1, -1, -1))
     elif tipo == 'aleatoria':
         lista = list(range(tamanho))
         random.shuffle(lista)
@@ -12,7 +13,19 @@ def gerar_lista(tamanho, tipo):
     else:
         raise ValueError("Tipo de lista desconhecido. Use 'ordenada', 'inversa' ou 'aleatoria'.")
 
-# Exemplo de uso
+def salvar_lista_em_arquivo(lista, nome_arquivo):
+    with open(nome_arquivo, 'w') as arquivo:
+        for item in lista:
+            arquivo.write(f"{item}\n")
+
+# Exemplos de uso
 lista_aleatoria_1000 = gerar_lista(1000, 'aleatoria')
 lista_ordenada_10000 = gerar_lista(10000, 'ordenada')
 lista_inversa_50000 = gerar_lista(50000, 'inversa')
+
+# Salvando as listas em arquivos
+salvar_lista_em_arquivo(lista_aleatoria_1000, 'lista_aleatoria_1000.txt')
+salvar_lista_em_arquivo(lista_ordenada_10000, 'lista_ordenada_10000.txt')
+salvar_lista_em_arquivo(lista_inversa_50000, 'lista_inversa_50000.txt')
+
+print("Arquivos gerados com sucesso.")
